dbb58cf443856d99d0d24cfd04a71cb41454209f
gaffOverflow <gaffar.ibrahim18@gmail.com>
2023-04-15 21:27:13 +0100

    commenting...

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// import information
+// import informations
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 
@@ -9,61 +9,59 @@ import {
   onValue,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
-// function run() {
-// fetch database URL
+// fetch database URL and save it in an object variable
 const appSettings = {
   databaseURL: "https://task-scape-default-rtdb.firebaseio.com",
 };
 
-// setup communication between firebase database and our project
-// connect project from our database using imported function to connect to database
+// line 1 - initialize app to link firebase database and our project
+// run    - console.log(app) ==> test run to confirm firebase database is connected
+// line 2 - connect project to database with imported function
 const app = initializeApp(appSettings);
-// console.log(app) ==> check if project is connected to firebase database
-
 const database = getDatabase(app);
 const listInDB = ref(database, "list");
-// }
-// run()
 
-// assign variables to html elements required for javascript
+// assign variables to html elements
 const input = document.querySelector("input");
 const button = document.querySelector("#button");
 const taskContainer = document.querySelector(".taskContainer");
 
-// write a function to get whatever text in input field
+// line 1 - write a function to get whatever text in input field
+// line 2 - console.log(input.value) ==> run to confirm it's working
+// line 3 - callback function from firebase DataBase to push whatever text in input field to DataBase
+// run    - console.log(inputText);
+// line 4 - callback function to clear whatever text in input field
 button.addEventListener("click", () => {
-  // console.log(input.value) ==> run to confirm it's working
   let inputText = input.value;
-
-  // callback function from firebase DB to push whatever text in input field to DB
   push(listInDB, inputText);
 
-  // console.log(inputText);
-
-  // callback function from to push whatever text in input field to to listItem
-  // appendList(inputText);
-
-  // callback function to clear whatever text in input field
   clearInputField();
 });
 
+// line 1 - extract list in database with the onValue function
+// run    - console.log(snapshot.val())===> test run
+// line 3 - callback function to clear whatever text in itemList to prevent       duplicate item
+// line 4 - ittirate over the listItem extracted from database
+// run    - console.log(listItem[i])===>test run
+// line 6 - callback function from to push whatever text in input field to to listItem
 onValue(listInDB, function (snapshot) {
-  // console.log(snapshot.val())
   let listItem = Object.values(snapshot.val());
-  // console.log(listItem)
-
-  taskContainer.innerHTML = "";
-
+  clearListItem();
   for (let i = 0; i < listItem.length; i++) {
-    // console.log(listItem[i])
     let currentList = listItem[i];
     appendList(currentList);
   }
 });
 
-// write a function to clear input field
+// line 1 - write a function to clear listItem -- prevent duplicate item
+// line 2 - set input field content to empty string
+function clearListItem() {
+  taskContainer.innerHTML = "";
+}
+
+//line 1 - write a function to clear input field
+//line 2 - set input field content to empty string
 function clearInputField() {
-  // set input field content to empty string
   input.value = "";
 }
 
